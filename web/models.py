feee87b7fc723728c0a7ce8ec65ccf3c072edbac
Evelina Derezhenko <113761562+Ev1k@users.noreply.github.com>
2023-11-05 23:50:02 +0300

    + models

--- web/models.py
+++ web/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=128)
+    text = models.TextField()
+    date = models.DateTimeField()
+    likes = models.ManyToManyField(User)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    date = models.DateTimeField()
+
+
+class New(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    photo = models.CharField(max_length=64)
+    title = models.CharField(max_length=128)
+    description = models.TextField()
+    date = models.DateTimeField()
+
+
+# class Like(models.Model):
+#     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+#     author = models.ForeignKey(User, on_delete=models.CASCADE)
