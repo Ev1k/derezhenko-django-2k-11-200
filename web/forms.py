d99befc55b187adf026a94861b21ef8fea49091d
Evelina Derezhenko <113761562+Ev1k@users.noreply.github.com>
2023-11-14 11:40:19 +0300

    auth, registration and adding posts added

---
+++ web/forms.py
@@ -0,0 +1,43 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+from web.models import Post
+
+User = get_user_model()
+
+
+class RegistrationForm(forms.ModelForm):
+    email = forms.EmailField()
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+    password2 = forms.CharField(widget=forms.PasswordInput())
+
+    def clean(self):
+        cleaned_data = super().clean()
+        if cleaned_data['password'] != cleaned_data['password2']:
+            self.add_error("password", "Пароли не совпадают")
+        return cleaned_data
+
+    class Meta:
+        model = User
+        fields = ("email", "username", "password", "password2")
+
+
+class AuthForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput())
+
+
+class AddPostForm(forms.Form):
+    title = forms.CharField()
+    text = forms.CharField()
+
+    class Meta:
+        model = Post
+        fields = ('title', 'text')
+
+    def save(self, commit=True):
+        self.instance.user = self.initial['user']
+        return super.save(commit)
+
+
---
+++ web/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path
+
+from web.views import main_view, registration_view, auth_view, logout_view, add_post_view, edit_post_view
+
+urlpatterns = [
+    path('', main_view, name="main"),
+    path("registration/", registration_view, name="registration"),
+    path("auth/", auth_view, name="auth"),
+    path("logout/", logout_view, name="logout"),
+    path("posts/add/", edit_post_view, name="posts_add"),
+    path("posts/<int:id>/", edit_post_view, name="posts_edit")
+]
